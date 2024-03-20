69d25b344a9b3f2e36cbea1978b5bf43e11caa72
kluless13 <100767970+kluless13@users.noreply.github.com>
2024-03-20 22:51:38 +0530

    kluless - oranj

--- klulessdotio/app/components/HeroSection.jsx
+++ klulessdotio/app/components/HeroSection.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const HeroSection = () => {
   return (
     <section>
-        <h1 className="text-white"> Illuminated by God.</h1>
+        <h1 className="text-white"> Illuminated by God</h1>
+        <h3 className="text-orange-700"> kluless </h3>
     </section> 
   );
 }
