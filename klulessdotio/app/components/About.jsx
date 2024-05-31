dc82c36b52a5e9e3341dd7acc7b9dfb47e606363
kluless13 <100767970+kluless13@users.noreply.github.com>
2024-05-31 15:16:39 +0530

    spacing 2

--- klulessdotio/app/components/About.jsx
+++ klulessdotio/app/components/About.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const About = () => {
   return (
-   <section className="flex flex-col items-center justify-center min-h-screen text-center px-12 py-14">
+   <section>
 	<p className="text-orange-600"> I am kluless. </p>
+	  <p className="text-orange-600"> angad@oranj.co.in </p>
    </section> 
   )
 }
