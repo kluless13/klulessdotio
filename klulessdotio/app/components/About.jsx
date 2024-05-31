0fb3d0f6b743420f3be8cf7ad2b9607023c0b89a
kluless13 <100767970+kluless13@users.noreply.github.com>
2024-05-31 15:14:18 +0530

    spacing

--- klulessdotio/app/components/About.jsx
+++ klulessdotio/app/components/About.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const About = () => {
   return (
-   <section>
-    <p className="text-orange-600"> I am kluless. </p>
+   <section className="flex flex-col items-center justify-center min-h-screen text-center px-12 py-14">
+	<p className="text-orange-600"> I am kluless. </p>
    </section> 
   )
 }
--- klulessdotio/app/components/HeroSection.jsx
+++ klulessdotio/app/components/HeroSection.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
 
 const HeroSection = () => {
   return (
-    <section>
-        <h1 className="text-white"> Illuminated by God</h1>
-	<h1 className="text-white"> Filho do céu</h1>
-	<h1 className="text-white"> благословленный небесами </h1>
-	<h1 className="text-white"> विश्व रक्षक </h1>
-	<h1 className="text-white"> சொர்க்கத்தின் மகன் </h1>
-    </section> 
+    <section className="flex flex-col items-center justify-center min-h-screen text-center">
+        <h1 className="text-white">Illuminated by God</h1>
+        <h1 className="text-white">Filho do céu</h1>
+        <h1 className="text-white">благословленный небесами</h1>
+        <h1 className="text-white">विश्व रक्षक</h1>
+        <h1 className="text-white">சொர்க்கத்தின் மகன்</h1>
+    </section>
   );
 }
 
-export default HeroSection
+export default HeroSection;
